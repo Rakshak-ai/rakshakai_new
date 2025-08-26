5f2363c7fec72b496e0b4a70f7078ce5985fcf84
Rakshak-ai <rakshakaisos@gmail.com>
2025-08-26 17:36:01 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/rakshak/ai/MainActivity.kt
+++ android/app/src/main/kotlin/com/rakshak/ai/MainActivity.kt
@@ -1,32 +1,6 @@
 package com.rakshak.ai
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            Surface(color = MaterialTheme.colorScheme.background) {
-                Greeting("RakshakAI")
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Greeting("RakshakAI")
+class MainActivity: FlutterActivity() {
 }
